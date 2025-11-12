ac2eb4cc4b6c3ea4ebd8f955403d5d57afb775fa
Aletia88 <abababmgmgmg@gmail.com>
2025-11-12 15:09:16 +0300

    fix:layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 // app/page.tsx
 'use client'
-import AboutUsDemo from './components/About';
-import AudioStoriesDemo from './components/AudioStoriesDemo';
-import Footer from './components/Footer';
-import Hero from './components/Hero';
-import HistoryDemo from './components/History';
-import LeadershipSection from './components/Leaders';
+import AboutUsDemo from '../components/About';
+import AudioStoriesDemo from '../components/AudioStoriesDemo';
+import Footer from '../components/Footer';
+import Hero from '../components/Hero';
+import HistoryDemo from '../components/History';
+import LeadershipSection from '../components/Leaders';
 
 
 export default function Home() {
